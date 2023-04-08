e599aa34cf46cd42419a8b1fc9b82bc9fb8fe37d
jbetancur21 <juan_betancur82191@elpoli.edu.co>
2023-04-07 21:59:46 -0500

    07/04/2022 21:59

--- Carrito/src/components/ListaProductos.jsx
+++ Carrito/src/components/ListaProductos.jsx
@@ -18,6 +18,22 @@ const ListaProductos = () => {
 			Descripcion: 'good Nike shoes',
 			url: 'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/cdb4e7b0-7772-41aa-82c0-4306f53050b6/zoom-fly-5-zapatillas-de-running-carretera-6PMLS5.png',
 		},
+		{
+			id: '3u',
+			Nombre: 'Nike Streetgato',
+			Precio: 180,
+			cantDisponible: 2,
+			Descripcion: 'Really Nike shoes',
+			url: 'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/c526742e-8259-44bd-a37e-d27cf4e29194/streetgato-botas-de-futbol-zhwVgz.png',
+		},
+		{
+			id: '4u',
+			Nombre: 'Zoom Freak 4',
+			Precio: 150,
+			cantDisponible: 3,
+			Descripcion: 'Really Nike good Nike shoes',
+			url: 'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/dd30bba7-ae65-4fc3-975c-454f9b8d7ddd/zoom-freak-4-zapatillas-de-baloncesto-8Hdw9M.png',
+		},
 	];
 	const [arrayProductos, setProducto] = useState(products);
 
@@ -26,16 +42,17 @@ const ListaProductos = () => {
 			<h2>Lista</h2>
 			<div>
 				{arrayProductos.map(item => {
-					return(
-					<div>
-						<img src={item.url} width="15%" />
-						<p>{item.Nombre}</p>
-						<p>{item.Precio}$</p>
-						<p>{item.cantDisponible}</p>
-						<p>{item.Descripcion}</p>
-						
-					</div>
-				)})}
+					return (
+						<div>
+							<h4>{item.Nombre}</h4>
+							<img src={item.url} width='15%' />
+							<p>Precio: {item.Precio}$</p>
+							<p>Unidades disponibles: {item.cantDisponible}</p>
+							<p>Descripción del Producto: {item.Descripcion}</p>
+							<hr />
+						</div>
+					);
+				})}
 			</div>
 		</div>
 	);
